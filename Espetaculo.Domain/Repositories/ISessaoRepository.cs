93b81ead855cb1b075810721eefdf47589e24897
carvalho57 <gabrielfeocarvalho57@gmail.com>
2020-10-22 15:06:37 -0300

    Adicionanddo metodo GetByDAte ao contrato ISessaoREpository

--- Espetaculo.Domain/Repositories/ISessaoRepository.cs
+++ Espetaculo.Domain/Repositories/ISessaoRepository.cs
@@ -5,6 +5,7 @@ using Espetaculos.Domain.Entities;
 namespace Espetaculos.Domain.Repositories {
     public interface ISessaoRepository {
         Sessao GetById(Guid id);        
+        IEnumerable<Sessao> GetByDate(DateTime time);
         void Add(Sessao sessao);
         IEnumerable<Poltrona> GetPoltronasByIds(Guid sessao, IEnumerable<Guid> poltronas);
         bool IsHorarioFree(DateTime horario, Guid salaId);
